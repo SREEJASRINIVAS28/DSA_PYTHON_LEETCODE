dc9c7a65d2f8cbfe6a4c690b1ec017ff6412e4e9
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-08-29 19:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (37.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int count(int n,vector<int> &dp){
+    if(n==0) return 1;
+    if(n==1) return 1;
+    if(dp[n]!=-1) return dp[n];
+    int left=count(n-1,dp);
+    int right=count(n-2,dp);
+    return dp[n]=left+right;
+}
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        int ans=count(n,dp);
+        return ans;
+    }
+};
