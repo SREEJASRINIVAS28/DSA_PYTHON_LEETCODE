00727c26e64e0afd17863faf71046c47767b91a3
SREEJASRINIVAS28 <srijagedela57@gmail.com>
2025-08-31 22:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+int MaximumSum(vector<int>&nums,int n,vector<int> &dp){
+    if(n==0) return nums[0];
+    if(n<0) return 0;
+    if(dp[n]!=-1) return dp[n];
+    int pick=nums[n]+MaximumSum(nums,n-2,dp);
+    int notPick=0+MaximumSum(nums,n-1,dp);
+    return dp[n]=max(pick,notPick);
+}
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];  // edge case
-
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            int pick = nums[i] + dp[i - 2];  // ✅ use dp[i-2]
-            int notPick = dp[i - 1];         // ✅ use dp[i-1]
-            dp[i] = max(pick, notPick);
-        }
-
-        return dp[n - 1];
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        int ans=MaximumSum(nums,n-1,dp);
+        return ans;
     }
 };
